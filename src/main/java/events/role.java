96f976fa284c9dc10a22f020f14cf0599e11b301
KochMat <1matthiaskoch@gmail.com>
2021-07-11 12:17:46 +0200

    Fix role.java

--- src/main/java/events/role.java
+++ src/main/java/events/role.java
@@ -11,13 +11,13 @@ import java.util.Objects;
 public class role extends ListenerAdapter {
     public void onGuildMemberJoin(@Nonnull GuildMemberJoinEvent event){
         event.getGuild().addRoleToMember(event.getMember(), Objects.requireNonNull(event.getGuild().getRoleById(859161321076555776L))).queue();
-        Objects.requireNonNull(event.getGuild().getTextChannelById(862331304142045205L)).sendMessage(event.getMember().getNickname() + " ist gejoint").queue();
+        Objects.requireNonNull(event.getGuild().getTextChannelById(862331304142045205L)).sendMessage(event.getMember().getEffectiveName() + " ist gejoint").queue();
         Objects.requireNonNull(event.getGuild().getVoiceChannelById(862716129608204328L)).getManager().setName(event.getGuild().getMemberCount() + " sitzen um's Feuer").queue();
 
 
     }
     public void onGuildMemberRemove(@Nonnull GuildMemberRemoveEvent event){
-        Objects.requireNonNull(event.getGuild().getTextChannelById(862331304142045205L)).sendMessage(Objects.requireNonNull(event.getMember()).getEffectiveName() + " ist geleavt").queue();
+        Objects.requireNonNull(event.getGuild().getTextChannelById(862331304142045205L)).sendMessage(event.getMember().getEffectiveName()+ " ist geleavt").queue();
         Objects.requireNonNull(event.getGuild().getVoiceChannelById(862716129608204328L)).getManager().setName(event.getGuild().getMemberCount() + " sitzen um's Feuer").queue();
         }
 }
